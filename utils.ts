75c943eea5834cc43f0e65762cd07ff0e6608b46
Taylor King <taylorking206@u.boisestate.edu>
2025-01-01 14:42:36 -0700

    front/back end

---
+++ utils.ts
@@ -0,0 +1,27 @@
+import { match } from 'ts-utils/match';
+import { ColType } from './types';
+
+export const checkStrType = (str: string, type: ColType): boolean => {
+    switch (type) {
+        case 'string':
+            return true;
+        case 'number':
+            return !Number.isNaN(+str);
+        case 'bigint':
+            return !Number.isNaN(+str) || BigInt(str).toString() === str;
+        case 'boolean':
+            return ['y', 'n', '1', '0', 'true', 'false'].includes(str);
+        default:
+            return false;
+    }
+};
+
+export const returnType = (str: string, type: ColType) => {
+    return match(type)
+        .case('string', () => str)
+        .case('number', () => +str)
+        .case('bigint', () => BigInt(str))
+        .case('boolean', () => ['y', '1', 'true'].includes(str))
+        .exec()
+        .unwrap();
+};
