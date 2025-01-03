85d5265d3a8a0529950c306710153741a2f9b8d6
taylor <taylorking206@u.boisestate.edu>
2025-01-02 18:29:17 -0700

    change package.json and and indes.ts

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import * as api from './api';
+import * as backend from './back-end';
+import * as frontend from './front-end';
+import * as tcp from './tcp';
+import * as types from './types';
+import * as utils from './utils';
+
+export { api, backend, frontend, tcp, types, utils };
